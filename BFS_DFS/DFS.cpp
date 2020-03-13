90ada9c73bf1c3342dd8a7a9bcaa7f0d0cf5022b
jooyena <reve.yenas2@gmail.com>
2020-03-14 01:45:15 +0900

    DFS BFS N_Queens

---
+++ BFS_DFS/DFS.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<list>
+#include<stack>
+
+using namespace std;
+void DFS(list<int> graph[], int v, stack<int> st){
+    int t;
+    bool visited[]={0,0,0,0};
+
+    while(!(st.empty())){
+        t=st.top();
+        st.pop();
+
+        if(visited[t-1]==true) continue;
+
+        cout<<t<<" ";
+
+        while(!(graph[t].empty())){
+            st.push(graph[t].back());
+            graph[t].pop_back();
+        }
+        visited[t-1]=true;
+    }
+    
+}
