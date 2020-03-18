5acf398b3c6abd18bb78fce7b7ec2c3bb295c2ef
jooyena <reve.yeans2@gmail.com>
2020-03-19 03:51:02 +0900

    done

--- others/codingTest_79.cpp
+++ others/codingTest_79.cpp
@@ -1,50 +1,57 @@
 #include<iostream>
 #include<vector>
+#include<string>
+#include<algorithm>
+
 #define SIZE 5
 using namespace std;
-
-bool visited[]={true,false,false,false,false};
+// << DP로 바꿔보기 >>
+bool visited[]={false,false,false,false,false};
 int array[SIZE]={7,3,2,5,8};
-int ADD(int visited[], int array[]){
-    int NUM=0;   
-    for(int i=0;i<SIZE;i++){
-        if(visited[i]==true) NUM+=array[i];
-    }
-    return NUM;
-}
-
+vector <string>res;
 bool search(int S, int N, vector<int> V){
-    int i=0;
-    int cur=0;
-
-for(int i=0;i<SIZE;i++){
-        if(S==N){
-            for(int j=0;j<SIZE;j++){
-                if(visited[j]==true) cout<<array[j]<<" "; //print
-            }
-            return true;
-        }
-
-        if(S<N){
-            N-=array[V.back()];
-            visited[V.back()]= false;
-            V.pop_back();
-        }
-        else if(S>N){
-            cur = V.back();
-            cout<<"2 : "<<cur<<" ";
+    string temp;
+    for(int i=0;i<SIZE;i++){
+        if(visited[i]==true) continue;
+        //cout<<"1 : "<<N<<endl;
+        //if(S<N){
+           // cout<<"들어가는 수 : "<<i<<" "<<endl;
             V.push_back(i);
-            N+=array[cur];
-            visited[cur]=true;
-            search(S,N,V);
+            visited[i]=true;
+            search(S+array[i],N,V);
+      //  }
+       //cout<<S<<" "; << Debug
+      //  if(visited[V.back()]==true){
+            //int next = V.back();
+            visited[V.back()]=false;
+            V.pop_back();
+          //  cout<<endl<<"나가는 수 : "<<next<<endl;
+            //visited[next]=false;
+           // S-=array[next];<<?? 이걸 왜 안넣어도 되는 거지
+            
+     //   } 
+        if(S==N){   
+            temp=" ";
+            for(int j=0;j<SIZE;j++){
+                if(visited[j]==true) temp+=to_string(array[j])+" ";//print
+            }
+            res.push_back(temp);
         }
-}
-    return false;
+    }
 }
 
 int main(){
     vector<int> v;
-    v.push_back(0);
-   if(search(15,0,v)) cout<<"yes";
-   else cout<<"NO";
+    vector<string>::iterator iter= res.begin();
+    search(0,18,v);
+    if(!res.size()){
+        cout<<"NO"<<endl;
+    }else{
+        cout<<"YES ";
+        sort(res.begin(),res.end());
+        res.erase(unique(res.begin(),res.end()),res.end());
+        for(iter=res.begin();iter!=res.end();++iter){
+            cout<<" ["<<*iter<<"]";
+        }
     }
+}
