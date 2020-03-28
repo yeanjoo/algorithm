c1d920c205bb3e5628311da346cc6a86d7719fd5
jooyena <reve.yeans2@gmail.com>
2020-03-28 23:19:43 +0900

    beakjoon

--- baekjoon/18788.cpp
+++ baekjoon/18788.cpp
@@ -1,30 +1,42 @@
 #include<iostream>
+#include<algorithm>
 #include<vector>
-
 #include<time.h>
 using namespace std;
 
 int swapity_swap(int N,int K, int A1,int A2,int B1,int B2){
-    int V[N];
+    vector<int> V;
+    int array[]={A1,A2,B1,B2};
+    int time=K%3;
+    sort(array,array+4);
 
-    for(int i=0;i<N;i++){
-        V[i]=i+1;
+// DC BA EF ,  DC FE AB ,  EF CD AB , EF BA DC  , AB FE DC  , AB CD EF ??? 3?
+//DC FE AB  , EF BA DC , AB CD EF
+if(B1<=A2||A1<=B2){
+    if(time==1){
+        //print array[2]->array[1]->array[3]->array[2]->array[0]-> array[1]
+    };
+    if(time==2){
+        //print array[2]->array[3]->array[1]->array[0]->array[2]-> array[1]
+    };
+    else {
+            //??????
     }
-
-    for(int i=0;i<K;i++){
-        for(int j=A1,l=A2;j<l;j++,l--){
-            swap(V[j],V[l]);
-        }
-        for(int j=B1,l=B2;j<l;j++,l--){
-            swap(V[j],V[l]);
-        }
-    }
+} //???? ? ??
+//else if(A2<B1||B2<A2) //???? ?? ? ???? ?? ?? ?? ?? ???? ????
+//else < // ??? ??? ?? ??? ? ?? ???? ??? ????  ??? ????
     
-    //PRINT
-    for(int i=0;i<N;i++){
-        cout<<V[i]<<" ";
-    }
+    
+    /* for(int i=0;i<N;i++){
+    //     V[i]=i+1;
+    // }
 
+    // for(int i=0;i<K;i++){
+    //     reverse(V[A1],V[A2]);
+    //     reverse(V[B1],V[B2]);
+    // }
+    //PRINT
+*/
 }
 int main(){
    // clock_t begin,end;
