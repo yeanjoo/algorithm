c7713c3e4e66f446367b4c4a2e9867f1ed3eaffe
jooyena <reve.yeans2@gmail.com>
2020-04-02 14:25:52 +0900

    RV 1463

--- baekjoon/1463.cpp
+++ baekjoon/1463.cpp
@@ -1,59 +1,27 @@
 #include<iostream>
-#include<queue>
+#include<vector>
 #include<bitset>
 using namespace std;
-#define SIZE 100001
-int array[]={2,3};
-
+#define SIZE 1000001
 
 int dp(int N){
-    
-   //long long dp[SIZE];
-    
-    
-    /*for(int i=0;i<N;i++){
-        dp[i]=i;
-    }
+      int dp[N+1];
+      dp[0]=dp[1]=0;
+      int count=0;
+      for(int i=1;i<N;i++){
 
-    for(int i=0;i<2;i++){
-        for(int j=array[i];j<N;j++){
-            dp[j]=min(dp[j],dp[j-array[i]]+1);
-        }
-        
-    }*/
-   // cout<<dp[N-1];
-
-    int  time=0;
-    queue<int> q;
-    bitset <SIZE> visited;
-    
-    visited.reset();//√ ±‚»≠
-    q.push(N);
-
-    while(!q.empty()){
-
-        int cur =q.front();
-        q.pop();
-
-        if(1==cur) cout<<visited[cur]-1;
-
-        if(cur/2 > -1 && !visited.test(cur)){
-            visited|=1<<(cur/2);
-            q.push(cur/2);
-        }
-
-        if(cur-1 < SIZE && !visited.test(cur)){
-            visited|=1<<(cur-1);
-            q.push(cur-1);
-        }
-
-        if(cur/3 > -1 && !visited.test(cur)){
-           visited|=1<<(cur/3);
-           q.push(cur/3);
-        }
-        time++;
-    }
-    return time;
+          dp[i]=dp[i-1]+1;
+          
+          if(dp[i]==0) break;
+          
+          if(dp[i]%2==0){
+              dp[i]=min(dp[i],dp[i/2]+1);
+          }
+          if(dp[i]%3==0){
+              dp[i]=min(dp[i],dp[i/3]+1);
+          }
+      } 
+      return dp[N];
 }
 
 int main(){
